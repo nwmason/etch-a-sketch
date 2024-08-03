2c3e64589133bc37ec63c34f8f2d3a12ff462cee
nwmason <[200~174368433+nwmason@users.noreply.github.com>
2024-08-03 13:35:27 -0400

    flex-basis now calculates with grid size changes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const etchScreen = document.querySelector(".etch-screen");
 
 function generateGrid(gridNumber = 16) {
+    let basis = 100 / gridNumber
+    console.log(basis);
+
     for (let c = 0; c < gridNumber; c++) {
         for (let r = 0; r < gridNumber; r++) {
             const etchPixel = document.createElement("div");
             etchPixel.setAttribute("class", "etch-pixel");
-            // etchPixel.setAttribute("style", 'flex-basis: ${100/gridNumber};')
+            etchPixel.style.flexBasis = `${basis}%`;
             etchScreen.appendChild(etchPixel);
         }      
     }
